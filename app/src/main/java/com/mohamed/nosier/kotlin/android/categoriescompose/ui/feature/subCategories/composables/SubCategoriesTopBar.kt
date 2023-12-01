ff2790cc74b1afb70a4a8694e647891e9c8ab15d
Mohamed_Nosire <mohamed.nosier@segmatek.com>
2023-12-01 04:28:24 +0200

    Update TopBar at Subcategory

--- app/src/main/java/com/mohamed/nosier/kotlin/android/categoriescompose/ui/feature/subCategories/composables/SubCategoriesTopBar.kt
+++ app/src/main/java/com/mohamed/nosier/kotlin/android/categoriescompose/ui/feature/subCategories/composables/SubCategoriesTopBar.kt
@@ -2,25 +2,30 @@ package com.mohamed.nosier.kotlin.android.categoriescompose.ui.feature.subCatego
 
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mohamed.nosier.kotlin.android.categoriescompose.R
 
 @Composable
 fun ReposTopBar(onBackButtonClicked: () -> Unit) {
-//    TopAppBar(
-//        title = { Text(text = stringResource(R.string.subCategories_screen_top_bar_title)) },
-//        navigationIcon = {
-//            IconButton(onClick = { onBackButtonClicked() }) {
-//                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
-//            }
-//        }
-//    )
+    TopAppBar(
+        elevation = 0.dp,
+        backgroundColor= Color.LightGray,
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = { onBackButtonClicked() }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
